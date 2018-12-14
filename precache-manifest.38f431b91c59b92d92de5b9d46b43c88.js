7bf04b51f0ca5f143f89330fc9951c0123b78f43
Isaac Oduh <ufuomaoduh@gmail.com>
2018-12-14 17:32:47 +0100

    Updates

---
+++ precache-manifest.38f431b91c59b92d92de5b9d46b43c88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "2e2427877ed933518c6f",
+    "url": "/contactmanager/static/js/main.2e242787.chunk.js"
+  },
+  {
+    "revision": "bb5f6b6f069eb0186460",
+    "url": "/contactmanager/static/js/1.bb5f6b6f.chunk.js"
+  },
+  {
+    "revision": "2e2427877ed933518c6f",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "bb5f6b6f069eb0186460",
+    "url": "/contactmanager/static/css/1.4b2788e8.chunk.css"
+  },
+  {
+    "revision": "4bac6c35d8666caefa8a4b3e8967aa17",
+    "url": "/contactmanager/index.html"
+  }
+];
